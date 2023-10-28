f163bf57f12e4fa91d7c831eab7d816310792b28
Matteo <Matteoshurdhaqi@gmail.com>
2023-10-28 18:05:39 +0200

    fetch card instagram

--- Fakestagram/script.js
+++ Fakestagram/script.js
@@ -1,26 +1,41 @@
 
+const baseUserUrl = 'http://dms.cyberdelia.eu/api/v1/user';
+const basePostsUrl = 'http://dms.cyberdelia.eu/api/v1/post';
 
-// axios.get('http://dms.cyberdelia.eu/api/v1/user', {
-//     params:{
-//         firstname: 'Eric',
-//         lastname: 'Cartman'
-//     }
-// })
-// .then(function (response) {
-//    console.log(response.data[0]);
-   
-// }).catch(function (e) {
-//     console.error(e);
-// })
+const userImg = document.querySelector('.user_img');
+const userName = document.querySelectorAll('.user_name');
+const cardImg = document.querySelector('.card_img');
+const cardDescription = document.querySelector('.card_desc');
 
 
-const baseUrl = 'http://dms.cyberdelia.eu/api/v1/user';
+axios.get(baseUserUrl)
+.then((res)=>{userHandler((res.data))})
+.catch((e)=>{
+    console.log(e);
+});
 
-const getUser = async()=>{
-    try{
-        const response = await axios.get('http://dms.cyberdelia.eu/api/v1/user');
-        console.log(response);
-    }catch(e){
-        console.log(e);
-    }
+axios.get(basePostsUrl)
+.then((res)=>{imgHandler(res.data)})
+.catch((e)=>{
+    console.log(e);
+})
+
+
+
+function userHandler(data){
+    
+    const user = {...data[0]}
+    
+    let markup = `<img src=${user.imgSrc} alt="${user.firstname}-${user.lastname}">`;
+    userImg.insertAdjacentHTML('beforeend', markup);
+    // userName.insertAdjacentHTML('afterbegin', `<strong>${user.firstname} ${user.lastname}</strong>`)
+    userName.forEach(element => {
+        element.insertAdjacentHTML('afterbegin', `<strong>${user.firstname} ${user.lastname}</strong>`)
+    });
+}
+function imgHandler(data) {
+    const post = {...data[0]};
+    let markup = `<img src=${post.imgSrc} alt="${post.description}">`;
+    cardImg.insertAdjacentHTML('beforeend', markup);
+    cardDescription.insertAdjacentText('afterend', ` ${post.description}`);
 }
